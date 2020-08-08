c905a0d0e2ec3f89ea02feac631a68875fa5c143
Faris1989 <69387170+Faris1989@users.noreply.github.com>
2020-08-08 18:04:33 +0300

    New IPython notebook

---
+++ Coursera_Capstone .py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# Capstone project
+
+
+# In[2]:
+
+
+import numpy as np
+import pandas as pd
+
+
+# In[3]:
+
+
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
